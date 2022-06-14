d06996ed6469cb6723943f876165e84a2a8c4711
Ali Zaryab Naeem <58973388+aleezaryabnaeem@users.noreply.github.com>
2022-06-14 15:53:40 +0500

    Content Update

--- Elysium.Web/Controllers/HomeController.cs
+++ Elysium.Web/Controllers/HomeController.cs
@@ -29,5 +29,10 @@ namespace Elysium.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult GetStarted()
+        {
+            return View();
+        }
     }
 }
